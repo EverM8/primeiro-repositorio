094347abfe4a980b45b43342011f2580621eaf68
EverM8 <evcarvalho93@gmail.com>
2022-09-15 17:55:18 -0300

    Aula 1 JavaScript

---
+++ index.js
@@ -0,0 +1,24 @@
+/** 
+ * ->Tipos primitivos
+ * ->Variáveis no Javascript
+ * ->Operadores matemáticos do Javascript
+ */
+
+// strings -> textos
+// number -> números
+// boolean -> booleano (true or false)
+
+console.log("Hello World")// serve para escrever mensagens no console
+console.log(6)
+console.log(45.78)
+console.log(true)
+console.log(false)
+
+//Variáveis
+let mensagem ="Minha mensagem"
+
+console.log(mensagem)
+
+mensagem='Novo texto' // variável sobrescrita
+
+console.log(mensagem)

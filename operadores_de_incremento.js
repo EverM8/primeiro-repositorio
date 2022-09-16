7bce93203805b744e0d373b8ca10abae69197eaf
EverM8 <evcarvalho93@gmail.com>
2022-09-16 16:42:45 -0300

    Operadores matemáticos e de incremento

---
+++ operadores_de_incremento.js
@@ -0,0 +1,20 @@
+alert('Código Funcionando!') //mostrar uma mensagem para o usuario
+const nomeUsuario = prompt('Olá usuário, qual o seu nome?') // mostra uma mensagem e recebe
+
+let num=Number(prompt('Bem vindo, '+nomeUsuario+'. Digite um número, por favor: '))
+//console.log(typeof num)// typeof fala o tipo de valor da variável 
+
+
+
+// Operadores de incremento
+//const sucessor=num++
+
+//alert('O sucessor de '+num+ ' é '+sucessor)
+console.log(num++)//operador pós-incremento
+console.log(num)
+console.log(++num)//operador de pré-incremento
+
+// operadores de decremento
+console.log(num--) //operador de pós-decremento
+console.log(num)
+console.log(--num) //operador de pre-decremento

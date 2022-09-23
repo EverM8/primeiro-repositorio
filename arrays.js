e9b3a2fee2dd7d8cd52f24816899cf1e41344ff1
EverM8 <evcarvalho93@gmail.com>
2022-09-23 16:52:37 -0300

    Introdução Arrays

---
+++ arrays.js
@@ -0,0 +1,17 @@
+/**
+ * Arrays ou Vetores é um tipo especial que te permite armazenar 
+ * diversos valores em apenas um loca
+ */
+
+// Criando um Array(new Array ou [])
+/*let arr = new Array()
+let arr3 = [] //mais usada
+let arr2 = new Array(5)
+Dentro de um Array eu posso ter vários tipos  de dados*/
+/**      0  1  2     3                                      4         5    6    7 */
+let arr=[10,15,9.8,'Eu sou um texto dentro de um array','Olá mundo',true,false,true]
+console.log(arr[6])
+arr[6]='outro texto'
+console.log(arr[6])
+arr[8]='Agora a posição 8 existe dentro do arr'
+console.log(arr)
